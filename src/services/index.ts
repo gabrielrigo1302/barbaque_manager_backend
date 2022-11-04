58d4c97ae063096982aa8c4aaa2a7027e59e1e13
Gabriel de Oliveira Rigo <gabrieloliveirarigo13@gmail.com>
2022-11-03 22:56:38 -0300

    a

--- src/services/index.ts
+++
@@ -1,9 +0,0 @@
-import BarbequeService from "./BarbequeService";
-import PersonService from "./PersonService";
-import UserService from "./UserService";
-
-export default {
-    BarbequeService,
-    PersonService,
-    UserService
-}
